3de79da48a517d3f018d4bc468f9e2d1001f8275
Julien Semaan <jul.semaan@gmail.com>
2019-07-24 12:51:16 -0400

    only add culprit if first stacktrace frame is defined

--- lib/elastic_apm/error_builder.rb
+++ lib/elastic_apm/error_builder.rb
@@ -49,7 +49,7 @@ module ElasticAPM
         error.log.stacktrace = stacktrace
       end
 
-      error.culprit = stacktrace.frames.first.function
+      error.culprit = stacktrace.frames.first ? stacktrace.frames.first.function : nil
     end
 
     # rubocop:disable Metrics/MethodLength, Metrics/AbcSize
